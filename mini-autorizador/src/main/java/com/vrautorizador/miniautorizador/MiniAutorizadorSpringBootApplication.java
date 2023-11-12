4a92c818d86bc5b0dff12ad086f0735af37f5696
Arthur Valentim de Aguiar <arthurvaguiar@gmail.com>
2023-11-12 11:57:49 -0300

    Projeto Spring Boot inicial
    Criado o projeto pelo Spring Initializr, descompactação e criação do .gitignore

---
+++ mini-autorizador/src/main/java/com/vrautorizador/miniautorizador/MiniAutorizadorSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.vrautorizador.miniautorizador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniAutorizadorSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniAutorizadorSpringBootApplication.class, args);
+	}
+
+}
---
+++ mini-autorizador/src/test/java/com/vrautorizador/miniautorizador/MiniAutorizadorSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrautorizador.miniautorizador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniAutorizadorSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
